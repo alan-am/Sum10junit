8b9f96f382b2fd7286d54b123631d5308d6f5dcf
FrancisJ04 <frajvelo@espol.edu.ec>
2025-07-30 13:35:27 -0500

    Add OperationsTest skeleton for unit testing

---
+++ sum10/src/test/java/com/example/OperationsTest.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void makeFormula() {
+    }
+
+    @Test
+    void solve() {
+    }
+}
